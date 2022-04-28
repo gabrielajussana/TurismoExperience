2e545978c9cdb6307aead24ce0b339a86df2efa4
Gabriela <69367338+gabrielajussana@users.noreply.github.com>
2022-04-27 23:32:25 -0300

    last commit

--- script.js
+++
@@ -1,7 +0,0 @@
-
-const menu = document.querySelector('.menu')
-const menuIcon = document.querySelector('.sideMenu img')
-
-menuIcon.addEventListener('click', function() {
-  menu.classList.toggle('active')
-})
